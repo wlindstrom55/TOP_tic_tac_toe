43cda04a53de503536d30fc86f2b90f8fdf5e395
wlindstrom55 <75394313+wlindstrom55@users.noreply.github.com>
2022-07-21 15:45:26 -0500

    finished basic styling and logic testing, cleaned up a lot of comments. Still could organize better, but project instructions are mostly finished up at this point

--- tictac.js
+++ tictac.js
@@ -14,34 +14,18 @@ let didIWin = 'not yet'; //'not yet', 'Yes', 'No', or 'draw'
 let outOfMoves = 'No'; //will be Yes or No, to determine when there are moves left to play
 
 
-//TO USE, MUST ASSIGN PLAYER TO VARIABLE - since it's a factory patter
+//TO USE, MUST ASSIGN PLAYER TO VARIABLE - since it's a factory pattern
 const Player = () => {
     let wins = 0;
     let losses = 0;
-    
-    //we want player name, a score after wins, methods to change scoreboard?
-    // const changeScore = (result) => {
-    //     if(result == 'Win') {
-    //         return wins++;
-    //     }
-    //     if(result == 'Loss') {
-    //         return losses++;
-    //     }
-    // }
     return { wins, losses }
 };
 
-const p = Player();
+const p = Player(); //global, should find a better place
 
 //module for gameboard, since we only need one
 const Gameboard = (() => {
     let board = []; 
-    const resetGameboard = () => {
-        count1 = 0, count2 = 0, count3 = 0, count4 = 0, count5 = 0, count6 = 0;
-        count7 = 0, count8 = 0, count9 = 0, count10 = 0, count11 = 0, count12 = 0;
-        count13 = 0, count14 = 0, count15 = 0, count16 = 0;
-    }
-
     const checkWin = () => {
         let count1 = 0, count2 = 0, count3 = 0, count4 = 0, count5 = 0, count6 = 0;
         let count7 = 0, count8 = 0, count9 = 0, count10 = 0, count11 = 0, count12 = 0;
@@ -157,45 +141,33 @@ const Gameboard = (() => {
     }
 
     const confirmWin = () => {
-        // if(outOfMoves == 'No'){
-            if(didIWin == 'not yet'){
-                msgr.innerHTML = 'Who could possibly win!?';
-            } else if(didIWin == 'No') {
-                msgr.innerHTML = 'You LOSE!';
-                //setTimeout(); //something here to delay before starting a new game?
-                //p.changeScore('Loss');
-                p.losses++;
-                //didIWin = 'not yet';
-            } else if(didIWin == 'Yes') {
-                msgr.innerHTML = 'You WIN!';
-                //p.changeScore('Win');
-                //didIWin = 'not yet';
-                p.wins++;
-            }
-        // } else {
-        //     if(didIWin == 'draw') {
-        //         msgr.innerHTML = 'There was a draw!';
-        //     }
-        // }
+        if(didIWin == 'not yet'){
+            msgr.innerHTML = 'Who could possibly win!?';
+        } else if(didIWin == 'No') {
+            msgr.innerHTML = 'You LOSE!';
+            p.losses++;
+        } else if(didIWin == 'Yes') {
+            msgr.innerHTML = 'You WIN!';
+            p.wins++;
+        }
     }
 
     const opponentPlay = () => {
+        let q = 0; //q will prevent infinite loops and be ONE of TWO checks to see if there is a draw.
         const rando = (max) => {
             return Math.floor(Math.random() * max);
         };
-        let q = 0; //q will prevent infinite loops and be our check to see if there is a draw.
-        for(let x = 0; board.length - 1; x++) { //does it for as many buttons as there are, but rando is bad
+        for(let x = 0; board.length - 1; x++) {
             let oppChoice = board[rando(8)];
             if(oppChoice.getAttribute('played') == 'false') {
                 oppChoice.innerHTML = 'O';
                 oppChoice.setAttribute('played', 'opp');
                 countOpp++;
-                checkWin(); //would it not work to have this?
+                checkWin();
                 return;
-            } else if(q == 50) { //if loop makes it to 50, let's say there probably isn't another button left. 
+            } else if(q == 50) { //if loop makes it to 50, let's say there probably isn't another button left to play 
                 if(oppChoice.getAttribute('played') == ('true') || ('opp')) {
-                    //call draw condition
-                    initiateDraw();
+                    initiateDraw(); //call draw condition
                     return;
                 }
             } else if(oppChoice.getAttribute('played') == 'true') {
@@ -207,7 +179,7 @@ const Gameboard = (() => {
             }
         }
     }
-    return { board, checkWin, opponentPlay, resetGameboard, confirmWin};
+    return { board, checkWin, opponentPlay, confirmWin };
 })();
 
 //displayController - will control the flow of the game - renders the contents of the board to the page
@@ -215,7 +187,7 @@ const displayController = (() => {
     const popBoard = () => { //would be cool to adjust size somehow
         let displayCount = 1;
         let toDelete = document.querySelector('.grid');
-        toDelete.remove(); //checks for existing grid and removes before recreating. to reset board.
+        toDelete.remove(); //checks for existing grid (in DOM) and removes before recreating. to help reset board.
         let grid = document.createElement('div');
         grid.classList.add('grid');
         container.append(grid);
@@ -225,9 +197,9 @@ const displayController = (() => {
             square.setAttribute('name', `square${displayCount}`);
             square.setAttribute('played', 'false');
             square.classList.add('square');
-            square.innerHTML = `${displayCount}`; //change at some point to default value, maybe no char
+            square.innerHTML = `${" "}`; //default square value
             grid.append(square);
-            Gameboard.board.push(square); //could encapsulate gameboard better. maybe a method that pushes
+            Gameboard.board.push(square); //added to array here
             square.addEventListener('click', () => {
                 if(square.getAttribute('played') == 'true') {
                     msgr.innerHTML = 'You already played there dog, try again';
@@ -247,16 +219,16 @@ const displayController = (() => {
     
     const play = (square) => {
         //need some way to choose the Xs or Os. maybe a button?
-        if((square.getAttribute('played') == 'true') ) { //stop after 5 turns. (&& countPlay < 5)
+        if((square.getAttribute('played') == 'true') ) {
             square.innerHTML = 'X';
             countPlay++;
-            if(countPlay > 3) { //after 3 turns, start check for a win
+            if(countPlay > 3) {
                 Gameboard.checkWin();
             }
-            if(didIWin == 'not yet') {
-                Gameboard.opponentPlay(); //attempt to keep opponent from continuously playing after a win or loss.
+            if(didIWin == 'not yet') { //keeps opponent from continuously playing after a win or loss.
+                Gameboard.opponentPlay();
             }
-            if (countPlay > 4) { //if you're playing your 3rd, then allow ONE of TWO draw conditions
+            if (countPlay > 4) { //if you're playing your 4th, then allow ONE of TWO draw conditions
                 outOfMoves = 'Yes';
             } 
         } else if (square.getAttribute('played') == 'opp') {
@@ -271,18 +243,13 @@ const displayController = (() => {
 })();
 
 startButton.addEventListener('click', () => {
-    Gameboard.resetGameboard(); //should reset the checker logic counters
-    Gameboard.board.splice(0,Gameboard.board.length); //should empty the array
-    displayController.popBoard(); //clears and resets the game board
+    Gameboard.board.splice(0,Gameboard.board.length); //this will empty the array
+    displayController.popBoard();
     msgr.innerHTML = 'Who Will Win?';
-    //Gameboard.checkWin.didIWin = 'not yet'; //does this work?
-    //Gameboard.didIWin = 'not yet';
-    //need some way to reset the value...
     displayController.updateScore();
     //below here, reset global variables
     countPlay = 1;
     countOpp = 1;
-    //displayCount = 1;
     didIWin = 'not yet';
     outOfMoves = 'No';
 });
